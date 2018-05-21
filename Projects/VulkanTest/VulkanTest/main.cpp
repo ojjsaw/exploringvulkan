5fc2d359f4d873801e7f4b26b254cab0faf9e22a
ojjsaw <ojjsaw@gmail.com>
2018-05-20 21:38:02 -0700

    before swap chain chapter

--- Projects/VulkanTest/VulkanTest/main.cpp
+++ Projects/VulkanTest/VulkanTest/main.cpp
@@ -4,9 +4,21 @@
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
+#include <vector>
+#include <set>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+
+//indices of the queue families that satisfy certain desired properties.
+struct QueueFamilyIndices {
+	int graphicsFamily = -1;
+	int presentFamily = -1;
+
+	bool isComplete() {
+		return graphicsFamily >= 0 && presentFamily >= 0;
+	}
+};
 
 class HelloTriangleApplication {
 public:
@@ -19,6 +31,49 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
+	VkDevice device;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkQueue graphicsQueue;
+	VkSurfaceKHR surface;
+	VkQueue presentQueue;
+
+	void createInstance() {
+
+		//optional, but it may provide some useful information to the driver to optimize for our specific application
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		//tells the Vulkan driver which global extensions and validation layers we want to use. 
+		//Global here means that they apply to the entire program and not a specific device
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		//Vulkan is a platform agnostic API, which means that you need an extension to interface with the window system.
+		//GLFW has a handy built - in function that returns the extension(s) it needs
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		//specify 1.Pointer to struct with creation info, 2.Pointer to custom allocator callbacks, 
+		//3. Pointer to the variable that stores the handle to the new object 
+		//i.e. everything Vulkan needs to create an instance
+		//check if the instance was created successfully
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
 
 	void initWindow() {
 
@@ -40,7 +95,126 @@ private:
 	}
 
 	void initVulkan() {
+		createInstance();
+		createSurface();
+		pickPhysicalDevice();
+		createLogicalDevice();
+	}
 
+	void createSurface() {
+		if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create window surface!");
+		}
+	}
+
+	void createLogicalDevice() {
+		//set up a logical device to interface with physical device
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+		std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+		std::set<int> uniqueQueueFamilies = { indices.graphicsFamily, indices.presentFamily };
+
+		//Vulkan lets you assign priorities to queues to influence the scheduling of command buffer
+		//execution using floating point numbers between 0.0 and 1.0.This is required even if there is only a single queue
+		float queuePriority = 1.0f;
+		for (int queueFamily : uniqueQueueFamilies) {
+			VkDeviceQueueCreateInfo queueCreateInfo = {};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = queueFamily;
+			queueCreateInfo.queueCount = 1;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+			queueCreateInfos.push_back(queueCreateInfo);
+		}
+
+		//set of device features that we'll be using
+		VkPhysicalDeviceFeatures deviceFeatures = {};
+
+		VkDeviceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = queueCreateInfos.data();
+		createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+
+		createInfo.pEnabledFeatures = &deviceFeatures;
+		createInfo.enabledExtensionCount = 0;
+	    createInfo.enabledLayerCount = 0;
+
+		//instantiate the logical device with a call to the appropriately named vkCreateDevice function.
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create logical device!");
+		}
+
+		//retrieve queue handles for each queue family.
+		//logical device, queue family, queue index and a pointer to the variable to store the queue handle in.
+		//Because we're only creating a single queue from this family, we'll simply use index 0
+		vkGetDeviceQueue(device, indices.presentFamily, 0, &presentQueue);
+	}
+
+	void pickPhysicalDevice() {
+
+		//querying just the number of graphics cards
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+		if (deviceCount == 0) {
+			throw std::runtime_error("failed to find GPUs with Vulkan support!");
+		}
+
+		//allocate an array to hold all of the VkPhysicalDevice
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		//we'll check if any of the physical devices meet the requirements that we'll add to that function.
+		for (const auto& device : devices) {
+			if (isDeviceSuitable(device)) {
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		// ensure that the device can process the commands we want to use
+		QueueFamilyIndices indices = findQueueFamilies(device);
+		return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+
+		//retrieving the list of queue families 
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		//VkQueueFamilyPropertiesstruct contains some details about the queue family, 
+		//including the type of operations that are supported and the number of queues that can be created based on that family. 
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		//find at least one queue family that supports VK_QUEUE_GRAPHICS_BIT
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies) {
+			if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+			}
+
+			VkBool32 presentSupport = false;
+			vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+			if (queueFamily.queueCount > 0 && presentSupport) {
+				indices.presentFamily = i;
+			}
+
+			if (indices.isComplete()) {
+				break;
+			}
+
+			i++;
+		}
+		
+		return indices;
 	}
 
 	void mainLoop() {
@@ -52,8 +226,18 @@ private:
 	}
 
 	void cleanup() {
-		//destroying it 
+		//destroy device
+		vkDestroyDevice(device, nullptr);
+
+		vkDestroySurfaceKHR(instance, surface, nullptr);
+
+		//allocation and deallocation functions in Vulkan have an optional allocator callback 
+		//that we'll ignore by passing nullptr
+		vkDestroyInstance(instance, nullptr);
+
+		//destroying window
 		glfwDestroyWindow(window);
+
 		//terminate glfw itself
 		glfwTerminate();
 	}
